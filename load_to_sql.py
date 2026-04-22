4336c1e8bbd28bb8f18f7a4d813912f5c0a14d50
Ay 'Yanah Anderson <ayyanahanderson@Ays-MacBook-Air.local>
2026-04-21 19:40:35 -0700

    hide credentials in .env file

--- load_to_sql.py
+++ load_to_sql.py
@@ -1,15 +1,17 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
-# Load cleaned data
+load_dotenv()
+password = os.getenv("DB_PASSWORD")
+
 df = pd.read_csv("amazon_fashion_cleaned.csv")
 df = df.drop(columns=["price_range"], errors="ignore")
 
-# Connect to MySQL - update password to yours
 engine = create_engine(
-    "mysql+mysqlconnector://root:MatchaLover@localhost/amazon_db")
+    f"mysql+mysqlconnector://root:{password}@localhost/amazon_db")
 
-# Load into table
 df.to_sql("products", con=engine, if_exists="replace", index=False)
 
 print("Data loaded successfully!")
